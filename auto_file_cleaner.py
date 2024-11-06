77ebfdc6d580ed40f3226170280ef67ff05d0220
Mouradbdb <beddibimourad@gmail.com>
2024-11-06 17:36:58 +0100

    Implemented a lock file mechanism to prevent multiple script instances from running simultaneously.

--- auto_file_cleaner.py
+++ auto_file_cleaner.py
@@ -1,8 +1,20 @@
 import os
 import time
+import sys
 
 # Set the folder where you want to store the files to be deleted!
 folder_path = r'C:\Temp_Files'
+
+lock_file = 'script.lock'
+
+# Check if the lock file exists, which means the script is already running
+if os.path.exists(lock_file):
+    print("Another instance of the script is already running.")
+    sys.exit()
+    
+# Create the lock file to indicate that the script is running
+with open(lock_file, 'w') as f:
+    f.write('')
 
 # Set the time limit (24 hours in seconds)
 time_limit = 24 * 60 * 60  # 24 hours in seconds you can changethe time (in seconds)
@@ -27,7 +39,12 @@ def delete_old_files():
                     print(f"Failed to delete {file_path}: {e}")
 
 # Run the script continuously
-while True:
-    delete_old_files()
-    # Sleep for 1 minute before checking again
-    time.sleep(60)
+try:
+    while True:
+        delete_old_files()
+        # Sleep for 1 minute before checking again
+        time.sleep(60)
+
+finally:
+    # Remove the lock file when the script finishes
+    os.remove(lock_file)
